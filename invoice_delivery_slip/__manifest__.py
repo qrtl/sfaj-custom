89d5c5ce45bc57710767e966ea379af26e8813ac
Aungkokolin1997 <lin@quartile.co>
2025-01-27 10:33:02 +0000

    [ADD] invoice_delivery_slip

---
+++ invoice_delivery_slip/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2025 Quartile Limited
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Invoice Delivery Slip",
+    "category": "Invoice",
+    "version": "16.0.1.0.0",
+    "author": "Quartile",
+    "website": "https://www.quartile.co",
+    "license": "AGPL-3",
+    "depends": ["account"],
+    "data": [
+        "reports/account_report.xml",
+    ],
+    "installable": True,
+}
