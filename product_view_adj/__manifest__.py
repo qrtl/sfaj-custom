bb955d94636e567bd8cb97f5f8cfbcce6f4e1f9d
Aungkokolin1997 <lin@quartile.co>
2025-01-31 02:38:37 +0000

    [ADD] product_view_adj

---
+++ product_view_adj/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2025 Quartile Limited
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Product View Adjust",
+    "category": "Product",
+    "version": "16.0.1.0.0",
+    "author": "Quartile",
+    "website": "https://www.quartile.co",
+    "license": "AGPL-3",
+    "depends": ["product"],
+    "data": [
+        "views/product_template_views.xml",
+    ],
+    "installable": True,
+}
---
+++ product_view_adj/readme/DESCRIPTION.md
@@ -0,0 +1,2 @@
+This module moves the default_code column before the name column in the
+product template list.
