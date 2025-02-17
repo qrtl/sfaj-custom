d34a6c4cd8341e646e6e86a19f81076b6c6fddb4
Yoshi Tashiro (Quartile) <tashiro@quartile.co>
2025-02-17 19:27:42 +0900

    [4921][ADD] account_move_contact_attribut, sale_order_contact_attribute (#4)

---
+++ account_move_contact_attribute/models/account_move.py
@@ -0,0 +1,11 @@
+# Copyright 2025 Quartile (https://www.quartile.co)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class AccountMove(models.Model):
+    _inherit = "account.move"
+
+    contact_person = fields.Char(tracking=True)
+    site_name = fields.Char(tracking=True)
---
+++ sale_order_contact_attribute/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2025 Quartile Limited
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Sale Order Contact Attributes",
+    "version": "16.0.1.0.0",
+    "category": "Sales/Sales",
+    "author": "Quartile",
+    "website": "https://www.quartile.co",
+    "depends": ["sale", "account_move_contact_attribute"],
+    "license": "AGPL-3",
+    "data": [
+        "views/sale_order_views.xml",
+    ],
+    "installable": True,
+}
