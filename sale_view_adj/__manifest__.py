eca61af6599a9f5aa451c79595584ae0249ac691
Aungkokolin1997 <lin@quartile.co>
2025-02-28 01:42:41 +0000

    [ADD] sale_view_adj

---
+++ sale_view_adj/__manifest__.py
@@ -0,0 +1,13 @@
+# Copyright 2025 Quartile (https://www.quartile.co)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Sale View Adj",
+    "category": "Sale",
+    "version": "16.0.1.0.0",
+    "author": "Quartile",
+    "website": "https://www.quartile.co",
+    "license": "AGPL-3",
+    "depends": ["sale"],
+    "data": ["views/sale_order_views.xml"],
+    "installable": True,
+}
---
+++ sale_view_adj/readme/DESCRIPTION.md
@@ -0,0 +1,1 @@
+This module shows the address of partner_shipping_id in the sales order.
