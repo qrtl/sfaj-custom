1d0e14936963090b7776f2e2f8cc4c37db019b92
Yoshi Tashiro (Quartile) <tashiro@quartile.co>
2025-03-09 09:24:11 +0900

    [4921][ADD] l10n_jp_summary_invoice_contact_attribute (#5)

---
+++ l10n_jp_summary_invoice_contact_attribute/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2025 Quartile (https://www.quartile.co)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Japan Summary Invoice Contact Attributes",
+    "version": "16.0.1.0.0",
+    "category": "Japanese Localization",
+    "author": "Quartile, Odoo Community Association (OCA)",
+    "website": "https://www.quartile.co",
+    "license": "AGPL-3",
+    "depends": [
+        "l10n_jp_summary_invoice",
+        "account_move_contact_attribute",
+    ],
+    "data": [
+        "reports/report_summary_invoice_templates.xml",
+    ],
+    "installable": True,
+}
