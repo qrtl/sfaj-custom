dd38a63b28cac07376954a52bdfce26173251fbe
Aung Ko Ko Lin (Quartile) <45355704+AungKoKoLin1997@users.noreply.github.com>
2025-04-18 19:33:24 +0700

    [5316][ADD] partner_view_adj (#12)

---
+++ partner_view_adj/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2025 Quartile (https://www.quartile.co)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Partner View Adjust",
+    "category": "Product",
+    "version": "16.0.1.0.0",
+    "author": "Quartile",
+    "website": "https://www.quartile.co",
+    "license": "AGPL-3",
+    "depends": ["account"],
+    "data": [
+        "views/res_partner_views.xml",
+    ],
+    "installable": True,
+}
