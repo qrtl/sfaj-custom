19cc64405ee7b745a4a696ae4d01b189e069872d
kanda999 <kanda@quartile.co>
2025-04-25 09:32:19 +0000

    [ADD] stock_security_adjust

---
+++ stock_security_adjust/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2024 Quartile Limited
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
+{
+    "name": "Stock Security Adjust",
+    "version": "16.0.1.1.0",
+    "author": "Quartile Limited",
+    "website": "https://www.quartile.co",
+    "category": "Security",
+    "license": "LGPL-3",
+    "depends": ["stock_account"],
+    "data": [
+        "security/stock_security.xml",
+        "data/ir_ui_menu_views.xml",
+        "views/stock_quant_views.xml",
+    ],
+    "installable": True,
+}
