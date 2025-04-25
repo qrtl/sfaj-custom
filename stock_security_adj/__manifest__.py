c62f95d109836af2a7102723b6c5a2b67c9d3b0f
Yoshi Tashiro <tashiro@quartile.co>
2025-04-25 13:22:34 +0000

    [5356][ADD] stock_security_adj

---
+++ stock_security_adj/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2025 Quartile (https://www.quartile.co)
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
+{
+    "name": "Stock Security Adjust",
+    "version": "16.0.1.0.0",
+    "author": "Quartile",
+    "website": "https://www.quartile.co",
+    "category": "Security",
+    "license": "LGPL-3",
+    "depends": ["stock"],
+    "data": [
+        "security/ir.model.access.csv",
+    ],
+    "installable": True,
+}
